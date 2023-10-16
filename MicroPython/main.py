61c08ad6ce2ef3286660a7a16302b467da508f61
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-10-16 00:51:35 +0000

    commit python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,66 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: 
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import neopixel
 
+np = np = neopixel.NeoPixel(pin16, 5)
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+print(np[0])
+np[0] = (0, 0, 0)
+print(np[1])
+np[1] = (0, 0, 0)
+print(np[2])
+np[2] = (0, 0, 0)
+print(np[3])
+np[3] = (0, 0, 0)
+np.show()
+
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        light_value = display.read_light_level()
+
+        if light_value <= 51:
+            np[0] = (0, 0, 255)
+            np.show()
+            sleep(1000)
+            np[0] = (0, 0, 0)
+            np.show()
+
+        if light_value >= 52:
+            if light_value < 104:
+                np[1] = (255, 255, 0)
+                np.show()
+                sleep(1000)
+                np[1] = (0, 0, 0)
+                np.show()
+
+        if light_value >= 104:
+            if light_value < 156:
+                np[2] = (255, 165, 0)
+                np.show()
+                sleep(1000)
+                np[2] = (0, 0, 0)
+                np.show()
+
+        if light_value >= 156:
+            if light_value < 208:
+                np[3] = (255, 0, 0)
+                np.show()
+                sleep(1000)
+                np[3] = (0, 0, 0)
+                np.show()
+
+        if light_value >= 208:
+            np[3] = (255, 0, 255)
+            np.show()
+            sleep(1000)
+            np[3] = (0, 0, 0)
+            np.show()
+        display.show(Image.HAPPY)
